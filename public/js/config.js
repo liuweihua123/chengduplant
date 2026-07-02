70aa1a82a88107cc275e700f5debc669f311ade2
刘伟华 <1643687526@qq.com>
2026-07-02 10:55:46 +0800

    Point frontend to Render backend

--- public/js/config.js
+++ public/js/config.js
@@ -1,1 +1,1 @@
-const BaseUrl = window.CHENGDU_API_BASE_URL || localStorage.getItem("CHENGDU_API_BASE_URL") || "https://api.example.com";
+const BaseUrl = window.CHENGDU_API_BASE_URL || localStorage.getItem("CHENGDU_API_BASE_URL") || "https://chengduplant.onrender.com";
